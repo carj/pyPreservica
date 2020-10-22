2fc3c36870c04f794937f6e486444a355810a8f3
carj <drjamescarr@gmail.com>
2020-10-22 14:10:22 +0100

    Add license metadata to init file

--- pyPreservica/__init__.py
+++ pyPreservica/__init__.py
@@ -1,12 +1,12 @@
-
 from .common import *
 from .contentAPI import ContentAPI
 from .entityAPI import EntityAPI
 from .uploadAPI import UploadAPI, simple_asset_package, complex_asset_package
 from .workflowAPI import WorkflowAPI
 
-
 __author__ = "James Carr (drjamescarr@gmail.com)"
 
 # Version of the Preservica API package
 __version__ = "0.8.5"
+
+__license__ = "Apache License Version 2.0"
